46c287c549bbe9ea219ee5959181d294e46c89bd
Laura Rolon <mrolonmartinez@udesa.edu.ar>
2021-11-01 10:38:24 -0300

    prueba

---
+++ src/screens/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+ import { View, Text } from 'react-native';
+function Card() {
+  return (
+<View> 
+  <Text> Hello World </Text> 
+</View> 
+);
+}
+export default Card;
